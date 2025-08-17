02c8eadc0cd63840c4e1a8b6f43090c4a793dd93
divinepet <ananasa80@gmail.com>
2025-08-17 10:54:31 +0300

    no ath

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
     const fetchAll = async () => {
       try {
         const baseUrls = [
-          'https://webws.365scores.com/web/games/fixtures?competitors=131,132,134',
+          'https://webws.365scores.com/web/games/fixtures?competitors=131,132',
           'https://webws.365scores.com/web/games/fixtures?competitors=5061,2378,5050,5054,2379,5028,2372',
           'https://webws.365scores.com/web/games/fixtures?competitions=5096', // FIFA Club World Cup
           'https://webws.365scores.com/web/games/fixtures?competitions=572', // UEFA Champions League
